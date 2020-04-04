bb867944b0338433ad9c597e31eaba5247b2147c
Brandon Frie <brandon@brandonfrie.com>
2020-04-04 14:31:42 -0500

    added a react app back in...

---
+++ Telemachus/TelemachusBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Telemachus
+{
+    public class TelemachusBehaviour : MonoBehaviour
+    {
+        IWebHost server;
+        
+        public void Awake()
+        {
+            server = new WebHostBuilder().Build();
+            server.Start();
+        }
+
+        public void OnDestroy()
+        {
+            server.StopAsync();
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
